1865571bed6372629ba2578bee9fd17aab00b721
Yusif Yusifov <komport@gmail.com>
2018-09-12 15:12:37 +0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,9 @@
 from telebot import TeleBot as tb # Telegram API
 
 class poster:
-   _token = "286048859:AAHmE0L8__k6ZcP10E4NIQMWs2dshqdS_Y4"
+   _token = "token"
    _url = "https://api.telegram.org/bot"
-   _channel_id = "@devopstalks"
+   _channel_id = "@tst" #channel link
    _bot = None
    def __init__(self):
       self._bot = tb(self._token)
